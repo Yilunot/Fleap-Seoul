749ceebac9e6688d6a7cb4ceb0b28b4ba7e290c7
Tony Liu <tonyliu05@hotmail.com>
2025-07-02 21:15:33 -0700

    NavBar Component

--- CS110/src/App.vue
+++ CS110/src/App.vue
@@ -5,14 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">Login</RouterLink>
       </nav>
     </div>
   </header>
@@ -34,7 +30,7 @@ header {
 nav {
   width: 100%;
   font-size: 12px;
-  text-align: center;
+  text-align: left;
   margin-top: 2rem;
 }
 
@@ -77,7 +73,6 @@ nav a:first-of-type {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
   }
