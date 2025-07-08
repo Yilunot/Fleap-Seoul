56793164bd90c7ea4956fcba99bffb2a228e15ad
Tony Liu <tonyliu05@hotmail.com>
2025-07-07 21:18:16 -0700

    LogInButton

--- CS110/src/views/HomeView.vue
+++ CS110/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import CreatePost from '@/components/CreatePost.vue';
+import LoginButton from '@/components/LoginButton.vue';
 </script>
 
 <template>
   <main>
     <CreatePost />
+    <LoginButton />
   </main>
 </template>
