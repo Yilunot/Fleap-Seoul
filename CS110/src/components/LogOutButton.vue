6638107b328f98aedc05a5f437ddd03a7f480753
Tony Liu <tonyliu05@hotmail.com>
2025-07-08 20:09:12 -0700

    LogOutButton

---
+++ CS110/src/components/LogOutButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="logOut" class="logout-button">
+        Log Out
+    </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(['logOut']);
+</script>
---
+++ CS110/src/views/UserProfileView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <UserStat :user="user" />
+    <Followers :users="followers" :canFollow="canFollow" :onFollow="onFollow" title="Suggested Followers" />
+  </div>
+</template>
+
+<script setup>
+import UserStat from '@/components/UserStats.vue';
+import Followers from '@/components/Followers.vue';
+
+defineProps(['user', 'followers', 'canFollow', 'onFollow']);
+</script>
